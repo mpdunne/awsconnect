0e21c739e8ee7537954335dac2e570c108edf45a
mpdunne <mpdunne@live.com>
2024-08-16 09:39:13 +0100

    Rename key --> key_file

--- awsconnect.sh
+++ awsconnect.sh
@@ -10,10 +10,10 @@ function awsstart {
 }
 
 function awsconnect {
-        key=$1
+        key_file=$1
         public_dns=$2
         echo "Connecting to ${public_dns}"
-        ssh -i $key "ubuntu@${public_dns}"
+        ssh -i $key_file "ubuntu@${public_dns}"
 }
 
 function awsgetdns {
@@ -22,8 +22,14 @@ function awsgetdns {
         echo $(aws ec2 describe-instances --region $region --filters "Name=instance-id,Values=${instance_id}" --query "Reservations[].Instances[].PublicDnsName" --output text)
 }
 
+function awsgetdnsbyname {
+        region=$1
+        name=$2
+        echo $(aws ec2 describe-instances --region $region --filters "Name=tag:Name,Values=${name}" --query "Reservations[].Instances[].PublicDnsName" --output text)
+}
+
 function awsgo {
-        key=$1
+        key_file=$1
         region=$2
         instance_id=$3
 
@@ -38,6 +44,6 @@ function awsgo {
         if [[ "$public_dns" == "" ]]; then
                 echo "Could not get the ip address of ${instance_id}, it is not reachable."
         else
-                awsconnect $key $public_dns
+                awsconnect $key_file $public_dns
         fi
 }
