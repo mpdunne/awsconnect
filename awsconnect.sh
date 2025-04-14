607fbd45682a9ef91fb1b4b6be529f99ecf8b97c
Michael Dunne <6329303+mpdunne@users.noreply.github.com>
2025-04-14 12:02:08 +0200

    Keep trying to start instance until successful.

--- awsconnect.sh
+++ awsconnect.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
 function awsstart {
-        region=$1
-        instance_id=$2
-        echo "Starting instance: $region $instance_id"
-        aws ec2 start-instances --region $region --instance-ids "$instance_id"
+    region=$1
+    instance_id=$2
+
+    echo "Starting instance: $region $instance_id"
+    aws ec2 start-instances --region "$region" --instance-ids "$instance_id" > /dev/null
+
+    while true; do
+        state=$(aws ec2 describe-instances \
+            --region "$region" \
+            --instance-ids "$instance_id" \
+            --query "Reservations[0].Instances[0].State.Name" \
+            --output text)
+
+        echo "Current state: $state"
+	if [ "$state" = "running" ]; then
+            echo "Instance is running."
+            break
+        fi
+
         echo "Waiting 10 seconds..."
         sleep 10
+    done
 }
 
 function awsconnect {
         key_file=$1
-	user=$2
+	remote_user=$2
         public_dns=$3
-        shift 2
+        shift 3
         echo "Connecting to ${public_dns}"
-        ssh -i $key_file "$@" "$user@${public_dns}"
+        ssh -i $key_file "$@" "$remote_user@${public_dns}"
 }
 
 function awsgetdns {
@@ -32,10 +48,10 @@ function awsgetdnsbyname {
 
 function awsgo {
         key_file=$1
-	user=$2
+	remote_user=$2
         region=$3
         instance_id=$4
-        shift 3
+        shift 4
 
         echo "Getting the ip address..."
         public_dns=$(awsgetdns $region $instance_id)
@@ -48,6 +64,6 @@ function awsgo {
         if [[ "$public_dns" == "" ]]; then
                 echo "Could not get the ip address of ${instance_id}, it is not reachable."
         else
-                awsconnect $key_file $user $public_dns "$@"
+                awsconnect $key_file $remote_user $public_dns "$@"
         fi
 }
